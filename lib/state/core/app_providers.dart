1776cc6764e159866c998434a30705b6074a893e
ghostheart5 <ghostheart131517@gmail.com>
2026-07-06 15:58:25 -0500

    refactor(state-core): simplify bootstrap and retire provider registry

--- lib/state/core/app_providers.dart
+++ lib/state/core/app_providers.dart
@@ -1,14 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-export 'package:fantastic_guacamole/data/di/services_providers.dart'
-    show appInitializerProvider;
-export 'package:fantastic_guacamole/state/core/provider_registry.dart'
-    show providerRegistryProvider;
 export 'package:fantastic_guacamole/state/core/state_bootstrap.dart'
     show stateBootstrapProvider;
 export 'package:fantastic_guacamole/state/providers/domain_usecase_providers.dart';
 export 'package:fantastic_guacamole/state/providers/energy_provider.dart';
-export 'package:fantastic_guacamole/state/providers/focus_task_provider.dart';
 export 'package:fantastic_guacamole/state/providers/learning_history_provider.dart';
 export 'package:fantastic_guacamole/state/providers/notification_provider.dart';
 export 'package:fantastic_guacamole/state/providers/paywall_provider.dart';
@@ -18,6 +13,7 @@ export 'package:fantastic_guacamole/state/providers/sync_provider.dart';
 export 'package:fantastic_guacamole/state/providers/task_provider.dart';
 
 const String onboardingCompleteStorageKey = 'onboarding_complete';
+const String onboardingContentVersionStorageKey = 'onboarding_content_version';
 
 final soundEnabledProvider = NotifierProvider<SoundEnabledNotifier, bool>(
   SoundEnabledNotifier.new,
