6b4a6d7757e9fdf0269803a371a15be860df39dc
final <ghostheart131517@gmail.com>
2026-07-31 22:10:50 -0500

    P3-2 closeout: lock media audio video sequencing guardrails

--- phase0_safe_preflight.ps1
+++ phase0_safe_preflight.ps1
@@ -122,6 +122,7 @@ $allowedExact = @(
   'test/release/p23_observability_hardening_contract_test.dart',
   'test/release/p24_connector_workflow_contract_test.dart',
   'test/release/p31_release_alm_parity_contract_test.dart',
+  'test/release/p32_media_audio_video_sequencing_contract_test.dart',
   'test/coverage_zero/use_case_command_coverage_test.dart',
   'docs/chronospark_p02_completion_verification_report.txt'
 )
